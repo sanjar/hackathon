b0e880c03b5e723da79a075bc8bab62fb4b33de6
saniar <msaniar@CGSCDSGH234RBJG.in623.corpintra.net>
2015-02-06 17:01:54 +0530

    helping hand

---
+++ HelpingHandApp/gen/com/mbrdi/helpinghand/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mbrdi.helpinghand;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ HelpingHandApp/src/com/mbrdi/helpinghand/StartActivity.java
@@ -0,0 +1,44 @@
+package com.mbrdi.helpinghand;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class StartActivity extends Activity {
+
+	WebView webView ;
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start);
+		webView = (WebView) findViewById(R.id.webView1);
+		WebSettings settings = webView.getSettings();
+		settings.setJavaScriptEnabled(true);
+		webView.setVerticalScrollBarEnabled(true);
+		webView.setHorizontalScrollBarEnabled(true);
+		webView.loadUrl("http://10.0.2.2:8080/HelpingHand/");
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
